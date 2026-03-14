8eb562536aaa4c4cae4d159d1f2b5d3fdab74c7c
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-03-14 21:13:18 +0530

    Updated Home Page with components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
 import Hero from "@/components/Hero"
+import { HowItWorks } from "@/components/HowItWorks";
+import { Features } from "@/components/Features";
+import { FeaturedTemplates } from "@/components/FeaturedTemplates";
+import { Pricing } from "@/components/Pricing";
 
 const page = () => {
   return (
     <div>
       <Hero />
+      <HowItWorks />
+      <Features />
+      <FeaturedTemplates />
+      <Pricing />
     </div>
   )
 }
