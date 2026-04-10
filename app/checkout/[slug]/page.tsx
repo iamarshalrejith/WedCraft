04f4f9f36a52aaf25af055642742d26688733651
Arshal Rejith <arshalrejithcodes@gmail.com>
2026-04-10 20:19:28 +0530

    Update checkout page

--- app/checkout/[slug]/page.tsx
+++ app/checkout/[slug]/page.tsx
@@ -281,7 +281,8 @@ export default function CheckoutPage({ params }: CheckoutPageProps) {
     template.slug === "mangal-utsav" ||
     template.slug === "royal-durbar" ||
     template.slug === "sunset-mandap" ||
-    template.slug === "kasi-yatra";
+    template.slug === "kasi-yatra" ||
+    template.slug === "bold-union";
 
   return (
     <div className="max-w-5xl mx-auto px-4 md:px-8 py-8 mt-10">
